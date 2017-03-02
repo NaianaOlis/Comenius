a236654bcd6b1d422f02df8e65b54761500e581e
NaianaOlis <naiana.olis@gmail.com>
2017-03-01 21:35:11 -0300

    01/03/2017 - reorganizando

---
+++ Documentacao/db/dadosAdmFicticio.sql
@@ -0,0 +1,5 @@
+Insert Into cidade (nome,uf_sigla) Values('Ficticia', 'AC');
+
+Insert Into endereco (logradouro,num,bairro,complemento,cep,cidade_idcidade) Values('Rua X',222,'YYY','ap 00',00000000,1);
+
+Insert Into usuario (idendereco,nome,email,datanasc,rg,rgemissor,ufrg,senha,cpf,genero,estadoCivil) Values(1,'Fulano de Tal','fulano@email.com',01/01/1900,111111,'ssp','AC','1111',22233344455,1,'Solteiro');
